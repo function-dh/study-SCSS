1bbf710e0f8b5cffbced335b2964f3f45f55e09d
김동현 <function_dh@stunitas.com>
2019-10-14 16:47:32 +0900

    eslint 업데이트

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent":[
+            "error",
+            4
+        ],
+        "no-unused-vars": 1,
+        "no-use-before-define": 1,
+        "no-redeclare": 1,
+        "no-console":0,
+    }
+};
